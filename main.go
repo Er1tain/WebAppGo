d5ce6f8577f3afdac357a86513a423d8385f2942
unknown <sanyapridava@mail.ru>
2024-08-07 02:03:54 +0300

    Фронт запускается в отдельной горутине

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"log"
 	"main/interfaces"
 	"main/routers"
+	"main/scripts"
 	user "main/services/User"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	//Запуск фронтенда
+	go scripts.RunClient()
+
 	app := fiber.New()
 	//Список контроллеров-сервисов
 	url_patterns := map[string]func(*routers.Router, string) interfaces.Controller{
@@ -19,5 +23,6 @@ func main() {
 	router := routers.InitRouter(app, "/")
 	router.Config(url_patterns)
 
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":3001"))
+
 }
